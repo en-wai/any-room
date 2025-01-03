28ddec021ef1d72df53c216d50f6e54d3f9f800c
en-wai <nyasare@hotmail.com>
2025-01-03 22:36:38 +0000

    Add logic for login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -88,18 +88,32 @@
       <div class="container">
           <div class="row justify-content-middle" style="margin-left: 397px;">
               <div class="col-md-6 mt-5">
-                      <form action="#" class="appointment-form" style="margin-top: -568px;">
+                      <form action="{{ route('login') }}" method="POST" class="appointment-form" style="margin-top: -568px;">
+                        
+                        @csrf
                           <h3 class="mb-3">Login</h3>
                           <div class="row">
                               <div class="col-md-12">
                                   <div class="form-group">
-                                      <input type="text" class="form-control" placeholder="Email">
+                                    <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                   </div>
                               </div>
                              
                               <div class="col-md-12">
                                   <div class="form-group">
-                                      <input type="password" class="form-control" placeholder="Password">
+                                    <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                   </div>
                               </div>
                               
@@ -107,7 +121,16 @@
                           
                               <div class="col-md-12">
                                   <div class="form-group">
-                                      <input type="submit" value="Login" class="btn btn-primary py-3 px-4">
+                                      {{-- <input type="submit" value="Login" class="btn btn-primary py-3 px-4"> --}}
+                                      <button type="submit" class="btn btn-primary">
+                                        {{ __('Login') }}
+                                    </button>
+    
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif  
                                   </div>
                               </div>
                           </div>
