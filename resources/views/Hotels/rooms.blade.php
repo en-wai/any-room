8c81a9daf98ba72be8e9a5d567a647ea7f23c896
en-wai <nyasare@hotmail.com>
2025-01-05 06:09:40 +0000

    Add feature to fetch rooms dynamically

--- resources/views/Hotels/rooms.blade.php
+++ resources/views/Hotels/rooms.blade.php
@@ -4,7 +4,7 @@
 
 
 
-<section class="hero-wrap hero-wrap-2" style="margin-top: -25px; background-image: url('{{ asset('images/image_2.jpg') }}');" data-stellar-background-ratio="0.5">
+<section class="hero-wrap hero-wrap-2" style="margin-top: -25px; background-image: url('{{ asset('assets/images/image_2.jpg') }}');" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text align-items-center justify-content-center">
@@ -19,26 +19,30 @@
   <section class="ftco-section bg-light ftco-no-pt ftco-no-pb">
           <div class="container-fluid px-md-0">
               <div class="row no-gutters">
-              <div class="col-lg-6">
-                  <div class="room-wrap d-md-flex">
-                      <a href="#" class="img" style="background-image: url(images/room-1.jpg);"></a>
-                      <div class="half left-arrow d-flex align-items-center">
-                          <div class="text p-4 p-xl-5 text-center">
-                              <p class="star mb-0"><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></p>
-                              <p class="mb-0"><span class="price mr-1">$120.00</span> <span class="per">per night</span></p>
-                              <h3 class="mb-3"><a href="rooms.html">Suite Room</a></h3>
-                              <ul class="list-accomodation">
-                                  <li><span>Max:</span> 3 Persons</li>
-                                  <li><span>Size:</span> 45 m2</li>
-                                  <li><span>View:</span> Sea View</li>
-                                  <li><span>Bed:</span> 1</li>
-                              </ul>
-                              <p class="pt-1"><a href="room-single.html" class="btn-custom px-3 py-2">View Room Details <span class="icon-long-arrow-right"></span></a></p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div class="col-lg-6">
+                @foreach ($getRooms as $room)
+                    <div class="col-lg-6">
+                        <div class="room-wrap d-md-flex">
+                            <a href="#" class="img" style="background-image: url( {{ asset('assets/images/'.$room->image.'')}} );"></a>
+                            <div class="half left-arrow d-flex align-items-center">
+                                <div class="text p-4 p-xl-5 text-center">
+                                    <p class="star mb-0"><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span><span class="fa fa-star"></span></p>
+                                    <p class="mb-0"><span class="price mr-1">{{$room->price}}</span> <span class="per">per night</span></p>
+                                    <h3 class="mb-3"><a href="rooms.html">{{$room->name}}</a></h3>
+                                    <ul class="list-accomodation">
+                                        <li><span>Max:</span> {{$room->num_persons}} Persons</li>
+                                        <li><span>Size:</span> {{$room->size}} m<sup>2</sup></li>
+                                        <li><span>View:</span> {{$room->view}}</li>
+                                        <li><span>Bed:</span> {{$room->num_beds}}</li>
+                                    </ul>
+                                    <p class="pt-1"><a href="room-single.html" class="btn-custom px-3 py-2">View Room Details <span class="icon-long-arrow-right"></span></a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                @endforeach
+              
+              {{-- <div class="col-lg-6">
                   <div class="room-wrap d-md-flex">
                       <a href="#" class="img" style="background-image: url(images/room-2.jpg);"></a>
                       <div class="half left-arrow d-flex align-items-center">
@@ -132,7 +136,7 @@
                           </div>
                       </div>
                   </div>
-              </div>
+              </div> --}}
           </div>
           </div>
       </section>
