bb5e943dcf8fc44cdc81dee08abf8c3e0d204d03
en-wai <nyasare@hotmail.com>
2025-01-09 12:55:19 +0000

    fix: Ensure success endpoint and view for PayPal payment redirection

--- resources/views/Hotels/redirect-to-pay.blade.php
+++ resources/views/Hotels/redirect-to-pay.blade.php
@@ -7,7 +7,7 @@
         <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
             <div class="col-md-7 ftco-animate">
-                <h2 class="subheading">Pay with Paypal</h2>
+                <h1 class="subheading">Pay with Paypal</h1>
                 <h1 class="mb-4"></h1>
             {{-- <p><a href="{{ route('home') }}" class="btn btn-primary">Go Home</a></p> --}}
             </div>
